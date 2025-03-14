ec67ff9275c7bd05a6ed2925c8fcc1b38f5f598f
dylan-indieka <dylan.indieka@student.moringaschool.com>
2025-03-14 00:10:43 -0700

    add files

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Remove the <main> element with id 'main'
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element with id 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Set the text content of the new <h1> element
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the new <h1> element to the body
+document.body.appendChild(newHeader);
